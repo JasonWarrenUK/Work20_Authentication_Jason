9d4df0dcafae5a19dab0bf1cfe03478df23f36f0
Jason Warren <contact.jwarren@gmail.com>
2024-10-20 13:49:18 +0100

    feat: create & log salting

--- hash.js
+++ hash.js
@@ -1,14 +1,55 @@
 const crypto = require("node:crypto");
 
-const password = {
-	user1: "hunter1",
-	user2: "hunter2",
+const pass = {
+	user1: "password1",
+	user2: "password2",
 }
 
-const hashed = {
-	user1: crypto.createHash("sha256").update(password.user1).digest("hex"),
-	user2: crypto.createHash("sha256").update(password.user2).digest("hex")
+const salt = {
+	user1: "salt1",
+	user2: "salt2",
 }
 
-console.log(`${password.user1}: ${hashed.user1}`);
-console.log(`${password.user2}: ${hashed.user2}`);
+function userInput(user) {
+	return {
+		userName: user,
+		password: pass[user],
+		salt: salt[user],
+	};
+}
+
+const users = {
+	user1: userInput("user1"),
+	user2: userInput("user2"),
+}
+
+function hash(text) {
+	return crypto.createHash("sha256").update(text).digest("hex")
+}
+
+function userOutput(user) {
+	return {
+		userName: users[user].userName,
+		passText: users[user].password,
+		saltText: users[user].salt,
+		mixText: users[user].password + users[user].salt,
+		passHash: hash(users[user].password),
+		saltHash: hash(users[user].salt),
+		mixHash: hash(users[user].password + users[user].salt),
+	}
+}
+
+function userLog(user) {
+	console.group(`=== ${userOutput(user).userName} ===`);
+		console.log(`Pass Text: ${userOutput(user).passText}`);
+		console.log(`Pass Hash: ${userOutput(user).passHash}`);
+		console.log(`Salt Text: ${userOutput(user).saltText}`);
+		console.log(`Mix Hash: ${userOutput(user).mixHash}`);
+	console.groupEnd();
+	return;
+}
+
+console.group("=== Passwords ===");
+	userLog("user1");
+	userLog("user2");
+console.groupEnd();
